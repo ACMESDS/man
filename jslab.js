78436477797e1a3f1ace671323df815940e62dc3
acmesds@somemail.com <acmesds@nowhere.org>
2018-01-16 15:30:40 -0600

    initial commit

---
+++ jslab.js
@@ -0,0 +1,65 @@
+// UNCLASSIFIED
+
+/**
+ * @class ENGINE
+ * @requires crypto
+ * @requires glwip
+ * @requires liegroup 
+ * @requires mathjs
+ * @requires digitalsignals
+ * @requires nodehmm
+ * @requires node-svd
+ * @requires jsbayes
+ * @requires recurrentjs
+ * @requires gamma
+ * @requires expectation-maximization
+ * @requires multivariate-normal
+ */
+
+var JSLAB = module.exports = {  // js-engine plugins 
+	MATH: require('mathjs'),
+	LWIP: require('glwip'),
+	DSP: require('digitalsignals'),
+	GAMMA: require("gamma"),
+	CRY: require('crypto'),
+	//RAN: require("randpr"),
+	SVD: require("node-svd"),
+	//RNN: require("recurrentjs"),
+	BNET: require("jsbayes"),
+	MLE: require("expectation-maximization"),
+	MVN: require("multivariate-normal"),
+	VITA: require("nodehmm"),
+	LOG: console.log,
+	Log: console.log,
+	JSON: JSON,			
+	MAT: function (ctx,code) {
+		var emctx = {};
+
+		Each(ctx, function (key, val) {
+			emctx[key] = (val && val.constructor == Array) 
+					? emctx[key] = EM.matrix(val)
+					: val;
+		});
+
+		EM.eval(code, emctx);
+
+		Each(emctx, function (key, val) {
+			ctx[key] = (val && val._data)
+				? val._data
+				: val;
+		});
+	}
+};
+
+var EM = JSLAB.MATH;
+
+EM.import({
+	isEqual: function (a,b) {
+		return a==b;
+	},
+	disp: function (a) {
+		console.log(a);
+	}
+});
+
+// UNCLASSIFIED
