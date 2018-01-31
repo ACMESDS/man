b0b652568b3584600b4ea02513597d32dd936716
acmesds@somemail.com <acmesds@nowhere.org>
2018-01-31 16:34:35 -0600

    rework chipaoi add voxelize and detector chipping

--- jslab.js
+++ jslab.js
@@ -21,11 +21,10 @@ var
 
 var 														// Totem modules
 	ENUM = require("enum"),
-	CHIP = require("chipper"),
 	Copy = ENUM.copy,
 	Each = ENUM.each;
 
-var JSLAB = module.exports = {  // js-engine plugins 
+var LAB = module.exports = {  // js-engine plugins 
 	libs: {
 		require: function (pk) {
 			console.log("jslab blocked package require");
@@ -51,6 +50,7 @@ var JSLAB = module.exports = {  // js-engine plugins
 			}
 		},
 		
+		CHIP: require("chipper"),
 		MATH: require('mathjs'),
 		LWIP: require('glwip'),
 		DSP: require('digitalsignals'),
@@ -76,7 +76,7 @@ var JSLAB = module.exports = {  // js-engine plugins
 					LWIP.write( query, data );
 
 				else
-					JSLAB.thread( function (sql) {
+					LAB.thread( function (sql) {
 						sql.query( query, data);
 						sql.release();
 					});
@@ -139,7 +139,7 @@ var JSLAB = module.exports = {  // js-engine plugins
 
 				else
 				if ( query.startsWith("/") )
-					JSLAB.fetcher( query, function (recs) {
+					LAB.fetcher( query, function (recs) {
 						if ( recs) {
 							recs.each( function (n,rec) {
 								if ( flush(ctx, rec, recs) ) feed(recs,cb);
@@ -152,7 +152,7 @@ var JSLAB = module.exports = {  // js-engine plugins
 					});
 
 				else 
-					JSLAB.thread( function (sql) {
+					LAB.thread( function (sql) {
 						var recs = [];
 
 						sql.each( "REG", query , [], function (rec) {  // feed recs
@@ -170,17 +170,261 @@ var JSLAB = module.exports = {  // js-engine plugins
 
 			else 
 				cb( null );
+		},
+		
+		DET: {
+			runDetector: function (detName, ctx, res) {
+				res(ctx);
+
+				LAB.thread( function (sql) {
+
+					if (ctx.train) 
+						LIBS.DET.trainDetector(sql, detName, ctx);
+
+					else {
+					}
+
+				});
+			},
+
+			trainDetector: function (detName, ctx, res) { //< gen  detector-trainging ctx for client with callback to res(ctx) when completed.
+
+				LAB.thread( function (sql) {
+					var vers = [0]; //ctx.Overhead ? [0,90] : [0];
+					var labels = ctx.Labels.split(",");
+
+					// train classifier
+					//	`python ${ENV.CAFENGINES}/train`
+
+					// train locator
+
+					labels.each(function (n,label) {
+
+						var posFilter = "digit +" + label,
+							newFilter = "digit -" + label;
+
+						sql.query(		// lock proofs
+							"START TRANSACTION", 
+							function (err) {	
+
+						sql.query( 		// allocate positives to this ctx
+							"UPDATE app.proofs SET ? WHERE ? AND ?",
+							[{posLock:detName}, {cat:"digit"}, {label:label}],
+						//	"UPDATE proofs SET ? WHERE MATCH (label) AGAINST (? IN BOOLEAN MODE) AND enabled",
+						//	[{posLock:detName},posFilter], 
+							function (err) {
+
+						sql.query(		// allocate negatives to this ctx
+							"UPDATE app.proofs SET ? WHERE ? AND NOT ?",
+							[{negLock:detName}, {cat:"digit"}, {label:label}],
+						//	"UPDATE proofs SET ? WHERE MATCH (label) AGAINST (? IN BOOLEAN MODE) AND enabled",
+						//	[{negLock:detName},negFilter], 
+							function (err) {
+
+						sql.query(
+							"SELECT * FROM app.proofs WHERE ? LIMIT 0,?",		// get allocated positives
+							[{posLock:detName},ctx.MaxPos],
+							function (err,posProofs) {
+
+						sql.query(								// get allocated negatives
+							"SELECT * FROM app.proofs WHERE ? LIMIT 0,?",
+							[{negLock:detName},ctx.MaxNeg],
+							function (err,negProofs) {
+
+						sql.query(			// end proofs lock.
+							"COMMIT", 
+							function (err) { 
+
+						Trace("PROOF ",[posProofs.length,negProofs.length], sql);
+
+						if (posProofs.length && negProofs.length) {	// must have some proofs to execute ctx
+
+							var	
+								posDirty = posProofs.sum("dirty"),
+								negDirty = negProofs.sum("dirty"),
+								totDirty = posDirty + negDirty,
+								totProofs = posProofs.length + negProofs.length,
+								dirtyness = totDirty / totProofs;
+
+							Trace('DIRTY', [dirtyness,ctx.MaxDirty,posDirty,negDirty,posProofs.length,negProofs.length], sql);
+
+							sql.query("UPDATE detectors SET ? WHERE ?",[{Dirty:dirtyness},{ID:ctx.ID}]);
+
+							if (dirtyness >= ctx.MaxDirty) {		// sufficiently dirty to cause ctx to execute ?
+
+								sql.query("UPDATE proofs SET dirty=0 WHERE least(?)",{posLock:detName,negLock:detName});
+
+								vers.each( function (n,ver) {  		// train all detector versions
+
+									var det = FLEX.clone(ctx);
+
+									det.Path = "det"+ver+"/"+label+"/"; 		// detector training results placed here
+									det.DB = "../db"+ver;						// positives and negatives sourced from here relative to ENV.DETS
+									det.posCount = posProofs.length;
+									det.negCount = negProofs.length;
+									det.posPath = det.Path + "positives.txt"; 	// + ENV.POSITIVES + (false ? jobFolder + ".positives" : det.PosCases + ".jpg");  		// .positives will disable auto-rotations
+									det.negPath = det.Path + "negatives.txt"; 	// + ENV.NEGATIVES + jobFolder + ".negatives";
+									det.vecPath = det.Path + "samples.vec";
+									det.posLimit = Math.round(det.posCount * 0.9); 	// adjust counts so haar trainer does not exhaust supply
+									det.negLimit = Math.round(det.negCount * 1.0);
+
+									det.link = det.Name.tag("a",{href:"/swag.view?goto=Detectors"}) + " " + det.posLimit + " pos " + det.negLimit + " neg";
+									det.name = det.Name;
+									det.client = log.client;
+									det.work = det.posCount + det.negCount;
+
+									Trace(`TRAIN ${det.Name} v${ver}`, sql);
+
+									var Execute = {
+										Purge: "rm -rf " + det.Path,
+										Reset: "mkdir -p " + det.Path,
+
+										// ************* NOTE 
+										// ****** Must pass bgcolor and bgthres as parms too - positive dependent
+										// ****** so must be dervied from image upload tags
+										Resample: 
+											`opencv_createsamples -info ${det.posPath} -num ${det.posCount} -w ${det.Width} -h ${det.Height} -vec ${det.vecPath}`,
+											//"opencv_createsamples -info $posPath -num $posCount -w $Width -h $Height -vec $Data/samples.vec",
+											//"opencv_createsamples $Switch $posPath -bg $negPath -vec $Vector -num $Samples -w $Width -h $Height -bgcolor 112 -bgthresh 5 -maxxangle $xRotate -maxyangle $yRotate -maxzangle $zRotate -maxidev $ImageDev",
+
+										Train: 
+											`opencv_traincascade -data ${det.Path} -vec ${det.vecPath} -bg ${det.negPath} -numPos ${det.posLimit} -numNeg ${de.negLimit} -numStages ${det.MaxStages} -w ${det.Width} -h ${det.Height} -featureType LBP -mode BASIC`
+											//"opencv_traincascade -data $Cascade -bg $negPath -vec $Vector -numPos $Positives -numNeg $Negatives -numStages $MaxStages -precalcValBufSize 100 -precalcIdxBufSize 100 -featureType HAAR -w $Width -h $Height -mode BASIC -minHitRate $MinTPR -maxFalseAlarmRate $MaxFPR -weightTrimRate $TrimRate -maxDepth $MaxDepth -maxWeakCount $MaxWeak"										
+									};
+
+									Trace((det.Execute||"").toUpperCase()+" "+det.name, sql);
+
+									/**
+									* Training requires:
+									*  	SAMPLES >= POSITIVES + (MAXSTAGES - 1) * (1 - STAGEHITR) * POSITIVES + NEGATIVES
+									* that is:
+									*	POSITIVES <= (SAMPLES-NEGATIVES) / (1 + (MAXSTAGES-1)*(1-STAGEHITR))
+									*
+									* Actual STAGES (from training log) <= MAXSTAGES 
+									* Desired HITRATE = STAGEHITR ^ MAXSTAGES --> STAGEHITR ^ (Actual STAGES)
+									* Desired FALSEALARMRATE = STAGEFAR ^ MAXSTAGES --> STAGEFAR ^ (Actual STAGES)
+									*
+									* The samples_zfullN100 file will always contain $NEGATIVES number of negative images.
+									*/
+
+									switch (det.Execute.toLowerCase()) {
+										case "purge": 
+										case "clear":
+											//sql.jobs().insert( "purge", Execute.Purge, det);
+											break;
+
+										case "reset":
+										case "retrain":
+
+											if (true) {						// gen training positives
+												var list = []; 
+
+												posProofs.each( function (n,proof) {
+													//list.push(proof.Name + " 1 0 0 " + (proof.Width-1) + " " + (proof.Height-1) );
+													list.push([det.DB+"/"+proof.name, 1, proof.left, proof.top, proof.width, proof.height].join(" "));
+												});
+
+												FS.writeFileSync(
+													`./public/dets/${det.posPath}`, 
+													list.join("\n")+"\n","utf-8");
+											}
+
+											if (true) {					 	// gen training negatives
+												var list = [];
+
+												negProofs.each( function (n,proof) {
+													list.push(det.DB+"/"+proof.name);
+												});
+
+												FS.writeFileSync(
+													`./public/dets/${det.negPath}`, 
+													list.join("\n")+"\n","utf-8");
+											}
+
+											if (true)
+												sql.jobs().insert( "reset", Execute.Reset, det, function () {
+													sql.jobs().insert( "sample", Execute.Resample, det, function () {
+														sql.jobs().insert( "learn", Execute.Train, det, function () {
+															if (res) res(det);
+														});
+													});
+												});
+
+											break;
+
+										case "resample":
+
+											sql.jobs().insert( "sample", Execute.Resample, det, function () {
+												sql.jobs().insert( "learn", Execute.Train, det, function () {
+													if (res) res(det);
+												});
+											});
+											break;
+
+										case "transfer":
+
+											sql.jobs().insert( "learn", Execute.Train, det, function () {
+												if (res) res(det);
+											});
+											break;
+
+										case "baseline":
+											break;
+
+										case "run":
+										case "detect":
+
+											if (FLEX.CHIP)
+											FLEX.CHIP.workflow(sql, {
+												detName: det.Name.replace(/ /g,"_"),
+												chanName: det.Channel,
+												size: det.Feature,
+												pixels: det.Pixels,
+												scale: det.Pack,
+												step: det.SizeStep,
+												detects: det.Hits,
+												infile: det.infile,
+												outfile: "/rroc/data/giat/swag/jobs",
+												ctx: {
+													client: req.client,
+													class: "detect",
+													name: det.Name,
+													link: det.Name.tag("a",{href:"/swag.view?goto=Detectors"}),
+													qos: req.profile.QoS,
+													priority: 1
+												}									
+											});
+
+											break;
+									}
+
+								});
+
+							}
+						}
+
+						}); // commit proofs
+						}); // select neg proofs
+						}); // select pos proofs
+						}); // update neg proofs
+						}); // update pos proofs
+						}); // lock proofs
+
+					});	// labels
+				}); ///sql thread
+			}
 		}
+		
 	},
 	
 	fetcher: null, //function () {},	// reserved for http fetcher
 	thread: null,
 	
 	config: function (opts) {
-		if (opts) Copy(opts,JSLAB);
+		if (opts) Copy(opts,LAB);
 	
 		/*
-		if (mysql = JSLAB.mysql)
+		if (mysql = LAB.mysql)
 			DSVAR.config({   // establish the db agnosticator 
 				mysql: Copy({ 
 					opts: {
@@ -201,11 +445,12 @@ var JSLAB = module.exports = {  // js-engine plugins
 	},
 	
 	plugins: {
-		voxelize: voxelize,
+		aois: aois,
 		news: news,
 		estmix: estmix,
 		genpr: genpr,
 		estpr: estpr,
+		haar: haar,
 		jsdemo1: function jsdemo1(ctx, res) {
 			LOG("jsdemo1 ctx", ctx);
 			//LOG("A="+ctx.A.length+" by "+ctx.A[0].length);
@@ -242,11 +487,12 @@ end
 //=========== Extend matlab emulator
 
 var 
-	LIBS = JSLAB.libs,
+	SQL = null, //< defined by engine
+	LIBS = LAB.libs,
 	GET = LIBS.GET,
 	PUT = LIBS.PUT,
 	LWIP = LIBS.LWIP,
-	LOG = console.log,
+	LOG = LIBS.LOG,
 	EM = LIBS.MATH;
 
 EM.import({
@@ -696,13 +942,13 @@ Return MLEs for random event process [ {x,y,...}, ...] given ctx parameters:
 	
 }
 
-function voxelize (ctx,res) {
-	res("ok");
-	JSLAB.thread( function (sql) {
-		CHIP.voxelize(sql, ctx, function (chip,sql) {
-			//cb(chip, null, sql);			
-		});	
-	});
+function aois(ctx,res) {
+	res(ctx);
+	LIBS.CHIP.voxelizeAOI(SQL, ctx);
 }	
+
+function haar(ctx,res) {
+	LIBS.DET.runDetector("haar", ctx, res);
+}
 
 // UNCLASSIFIED
