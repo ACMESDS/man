9e88907f3f19e8f92140cca797f255dee5b4e5c0
ACMESDS <g5120089@trbvm.com>
2019-06-27 22:59:08 -0400

    move genpr into man and refactor

--- man.js
+++ man.js
@@ -820,7 +820,7 @@ Copy({
 		var
 			saveKeys = $.saveKeys;
 
-		$.thread( (sql) => {
+		$.thread( sql => {
 			sql.getFields("app._stats", null, [], function (keys) {
 				keys.forEach(function (key) {
 					saveKeys[key] = true;
@@ -1075,7 +1075,7 @@ $.import({ // overrides
 	override: true
 });
 
-$({
+$.extensions = $.extensions = {
 	// misc and samplers
 	
 	isDefined: x => x ? true : false,
@@ -1365,22 +1365,336 @@ $({
 
 	// data generators
 	
-	xgen: function  (N, beta0, beta1, seed) {
-		var 
-			gen = GEN.create(),
-			u = seed ? gen.seed( seed ) : 0,
-			rand = gen.random,
-			X = $( N, (n,x) => x[n] = [2*rand()-1, 2*rand()-1] ),
-			Y = $( N, (n,y) => {
-				var
-					x = X[n],
-					p = 1.0/(1.0+ exp( -(beta0 + beta1[0]*x[0] + beta1[1]*x[1]) ));
+	gen: function (ctx, res) {
+	
+		function  genTest(N, beta0, beta1, seed) {
+			var 
+				gen = GEN.create(),
+				u = seed ? gen.seed( seed ) : 0,
+				rand = gen.random,
+				X = $( N, (n,x) => x[n] = [2*rand()-1, 2*rand()-1] ),
+				Y = $( N, (n,y) => {
+					var
+						x = X[n],
+						p = 1.0/(1.0+ exp( -(beta0 + beta1[0]*x[0] + beta1[1]*x[1]) ));
 
-				y[n] = (random()>=p) ? 1 : 0;
-				//Log(n,p,y[n], beta0, beta1, x,  beta0 + beta1[0]*x[0] + beta1[1]*x[1] );
-			});
+					y[n] = (random()>=p) ? 1 : 0;
+					//Log(n,p,y[n], beta0, beta1, x,  beta0 + beta1[0]*x[0] + beta1[1]*x[1] );
+				});
+
+			return {x: $.matrix(X), y: $.matrix(Y)};			
+		}
+		
+		function KL( solve, cb ) { // Karhouen-Loeve expansion with callback cb(pcs) || cb(null)
 			
-		return {x: $.matrix(X), y: $.matrix(Y)};
+			function getpcs(model, coints, dim, cb) {  // get pcs with callback cb(pcs) || cb(null)
+
+				function genpcs(coints, model, dim, cb) {  // make pcs with callback cb(pcs = {values,vectors,ref})
+					function evd( models, coints, dim, cb) {   // eigen value decomp with callback cb(pcs)
+						models.forEach( function (model) {	// enumerate over all models
+							coints.forEach( (coints) => {	// enumerate over all coherence intervals
+								$( `
+t = rng(-T, T, 2*N-1);
+Tc = T/M;
+xccf = ${model}( t/Tc );
+Xccf = xmatrix( xccf ); 
+R = evd(Xccf); `,   
+								{
+									N: dim,
+									M: coints,
+									T: 50
+								}, ctx => {
+
+									if (solve.trace)  { // debugging
+										$(`
+disp({
+M: M,
+ccfsym: sum(Xccf-Xccf'),
+det: [det(Xccf), prod(R.values)],
+trace: [trace(Xccf), sum(R.values)]
+})`, ctx);
+									}
+									
+/*
+basis: R.vectors' * R.vectors,
+vecres: R.vectors*diag(R.values) - Xccf*R.vectors,
+*/
+									cb({  // return computed stats
+										model: model,
+										intervals: coints,
+										values: ctx.R.values._data,
+										vectors: ctx.R.vectors._data
+									});
+								});
+							});
+						});
+					}
+
+					evd( [model], [coints], dim, function (pcs) {
+
+						var 
+							vals = pcs.values,
+							vecs = pcs.vectors,
+							ref = $.max(vals);
+
+						pcs.ref = ref;
+						pcs.dim = dim;
+						
+						Log(">>>evded pcs", vals.length );
+						
+						$.thread( sql => {
+							sql.beginBulk();
+
+							vals.forEach( (val, idx) => {  // save pcs
+								var
+									save = {
+										correlation_model: model,
+										coherence_intervals: coints,
+										eigen_value: val,	// val/ref
+										eigen_index: idx,
+										ref_value: ref,
+										max_intervals: dim,
+										eigen_vector: JSON.stringify( vecs[idx] )
+									};
+
+								//Log(save);
+
+								sql.query("INSERT INTO app.pcs SET ? ON DUPLICATE KEY UPDATE ?", [save,save] );
+							});
+
+							sql.endBulk();
+							sql.release();
+							Log(">>>>saved pcs");
+						});
+						
+						cb( pcs );  // forward the saved pcs
+					});
+				}
+
+				function findpcs( coints, model, lims, cb ) { // get pcs with callback cb(pcs = {values,vectors,ref})
+					$.thread( sql => {
+						sql.query(
+							"SELECT *, abs(? - coherence_intervals)  AS coeps FROM app.pcs WHERE coherence_intervals BETWEEN ? AND ? AND eigen_value / ref_value > ? AND least(?,1) ORDER BY coeps desc,eigen_value", 
+							[ coints, coints*(1-lims.coints), coints*(1+lims.coints), lims.mineig, {
+								max_intervals: lims.dim, 
+								correlation_model: model
+							}],
+							(err, pcs) => {
+								if ( err ) 
+									Log( err );
+
+								else {
+									var vals = [], vecs = [], dim = lims.dim, ref = pcs.length ? pcs[0].ref_value : 0;
+
+									pcs.forEach( (pc) => {
+										if ( vals.length < dim ) {
+											vals.push( pc.eigen_value );
+											vecs.push( JSON.parse( pc.eigen_vector ) );
+										}
+									});
+
+									cb({
+										values: vals,
+										vectors: vecs,
+										dim: dim,
+										ref: ref
+									});
+								}
+						});
+		
+						Log(q.sql);
+						sql.release();
+					});
+				}
+
+				findpcs( solve.coints, solve.model, { 
+					coints: 0.1,
+					mineig: solve.mineig,
+					dim: solve.dim
+				}, pcs => {
+					Log(">>>>found pcs");
+					if ( pcs.values.length )   // found pcs so send them on
+						cb( pcs );
+					
+					else  // try to generate pcs
+						genpcs( coints, model, dim, pcs => {  
+							Log(">>>>gened pcs", pcs.values.length);
+							if ( pcs.values.length )
+								findpcs( solve.coints, solve.model, { // must now find pcs per limits
+									coints: 0.1,
+									mineig: solve.mineig,
+									dim: solve.dim
+								}, pcs => {
+									if ( pcs.values.length) 
+										cb( pcs );
+									
+									else
+										cb(null);
+								});
+							
+							else
+								cb( null );
+						});
+						/*
+						SQL.query(
+							"SELECT count(ID) as Count FROM app.pcs WHERE least(?,1)", {
+								max_intervals: Mmax, 
+								correlation_model: model
+							}, 
+							function (err, test) {  // see if pc model exists
+
+							//Log("test", test);
+							if ( !test[0].Count )  // pc model does not exist so make it
+								genpcs( Mmax, Mwin*2, model, function () {
+									findpcs( sendpcs );
+								});
+
+							else  // search was too restrictive so no need to remake model
+								sendpcs(pcs);
+								
+						});*/
+				});
+			}
+	
+			// Should add a ctx.Shortcut parms to bypass pcs and use an erfc model for eigenvalues.
+
+			if ( solve.model )
+				getpcs( solve.model, solve.coints, solve.dim, pcs => {
+					pcs.mean = solve.mean;
+					cb(pcs);
+				});
+			
+			else
+				cb( null );
+		}
+		
+		function genProc(opts, cb) {
+			var ran = new $.RAN(opts);  // create a random process compute thread
+
+			ran.pipe(cb);   // run process and capture results
+		}
+		
+		var opts = { // supervisor config 
+			N: ctx.Members,  // ensemble size
+			symbols: ctx.Symbols,  // state symbols
+			
+			wiener: ctx.type_Wiener, // walks
+			markov: ctx.type_Markov, // trans probs
+			gauss: ctx.type_Gauss, // [mean count, coherence intervals]
+			bayes: ctx.type_Bayes, // equlib probs
+			ornstein: ctx.type_Ornstein,   // theta,  a = sigma / sqrt(2*theta)
+			
+			dt: 1/ctx.Nyquist, // oversampling factor
+			steps: ctx.Steps, // process steps
+			emP: ctx.emProbs,  	// mixing/emission/observation parms
+			batch: ctx.Batch || 0,   // supervised learning every batch steps
+			filter: function (str, ev) {  // filter output events
+				switch ( ev.at ) {
+					case "jump":
+						var 
+							ys = ev.obs || [];
+
+						str.push({
+							at: ev.at,  // step name
+							t: ev.t, // time sampled
+							u: ev.state,   // state occupied
+							n: ev.index, 	// unique identifier
+							x: ys[0],  	// lat
+							y: ys[1],  	// lon
+							z: ys[2] 	// alt
+						});
+						break;
+
+					case "_step":
+						if (walking) {
+							var ev = { 
+								at: ev.at,
+								t: ran.t,
+								u: 0,
+								k: 0,
+								n: 0
+							};
+
+							ran.WU.each(function (id, state) {
+								ev[ labels[id] || ("w"+id) ] = state;
+							});
+
+							str.push(ev);
+							//Log(ev);
+						}
+
+						else
+							ran.U.each( function (index, state) {
+								var ys = ran.Y[index];
+								str.push({ 
+									at: ev.at,  // step name
+									t: ran.t, // time sampled
+									u: state,   // state occupied
+									n: index, 	// unique identifier
+									x: ys[0],  	// lat
+									y: ys[1],  	// lon
+									z: ys[2] 	// alt
+								});
+							});
+
+						break;
+
+					case "_end":
+						Log(ev);
+						
+					case "batch":
+					case "config":
+					case "end":
+						str.push(ev);
+						break;
+
+					default:
+						//str.push(ev);
+				}			
+			}  // event saver 
+		};
+
+		if (gauss = opts.gauss) {	// generate gaussian process using exact pcs or approx negbin
+			var 
+				N = ctx.Steps, 
+				T = N,
+				dt = 1/ctx.Nyquist;
+			
+			if (gauss.model && gauss.coints)	// exact using pcs with specified coherence intervals
+				KL({  // parms for Karhunen Loeve solver
+					trace: false,   // eigen debug
+					T: T,  // observation interval  [1/Hz]
+					coints: gauss.coints , // coherence intervals
+					mean: gauss.mean * dt / T, // mean events over sample time
+					dim: gauss.dim || N,  // max coherence intervals when pcs are generated
+					model: gauss.model || "sinc",  // assumed correlation model for underlying CCGP
+					mineig: gauss.mineig || 0.1	// min eigen/ref level (typically >= 0.05 to use stable eigenvectors)
+				}, pcs => {
+
+					if (pcs) {  // use eigen expansion to generate counts
+						opts.gauss = pcs;
+						
+						genProc(opts, res);
+					}
+
+					else 
+						res( null );
+				});
+			
+			else { // approx via a K-state MCMC/MH with negbin equlib probs specified by mean and coints
+				Log("mh/mcmc tbd");
+				opts.bayes = gparms.coints ? [] : []; // negbin || poisson
+				genProc(opts, res);
+			}	
+		}
+		
+		else
+		if (beta = ctx.beta) 
+			res( genTest(ctx.N, beta[0], beta[1], ctx.seed) );
+		
+		else
+			genProc(opts, res);
+		
 	},
 	
 	// linear algebra
@@ -1692,7 +2006,9 @@ psd = abs(dft( ccf )); psd = psd * ccf[N0] / sum(psd) / df;
 		else
 			Log( a );
 	}
-});
+};
+
+$( $.extensions );
 
 [ // add Jimp methods
 	function save( file ) {
